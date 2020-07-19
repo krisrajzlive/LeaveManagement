eaabbaac8e0d6ced3b28ca5d8ea89d5309710301
krisrajz <krisrajz@gmail.com>
2020-07-19 18:01:32 +0530

    Roles

--- LeaveManagement/Controllers/LeaveTypesController.cs
+++ LeaveManagement/Controllers/LeaveTypesController.cs
@@ -1,17 +1,16 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AutoMapper;
+﻿using AutoMapper;
 using LeaveManagement.Contracts;
 using LeaveManagement.Data;
 using LeaveManagement.Models;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LeaveManagement.Controllers
 {
+    [Authorize(Roles = "Administrator")]
     public class LeaveTypesController : Controller
     {
         private readonly ILeaveTypeRepository _repo;
